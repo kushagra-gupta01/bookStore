7bd9473d52ecb22f888e0d03c32dab9a3cba903c
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-09-01 23:20:43 +0530

    fixed:6th commit

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,13 +1,15 @@
 package config
-import(
+
+import (
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	_ "gorm.io/driver/mysql"
 )
 var(
 	db* gorm.DB
 )
 func connect(){
-	d,err := gorm.Open("mysql","akhil:Axlesharma@12/simplerest?charset=utf8&parseTime=true&loc=Local")
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})  #docs
+	d,err := gorm.Open(mysql.Open("akhil:Axlesharma@12/simplerest?charset=utf8&parseTime=true&loc=Local"),&gorm.Config{})
 	if err!=nil{
 		panic(err)	
 	}
